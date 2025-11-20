9c3cd141d6ed78930485db36fa58108cdbdbba76
Miguel Manzo <miguelmanzo@MacBookAir.hsd1.ca.comcast.net>
2025-11-19 17:28:07 -0800

    added code to openmp for parallelization

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -32,6 +32,36 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
+   //taken from lecture 22
+   /*
+   for row in 0,M:
+      for col in 0,N:
+         index = col + row * N
+         A[index] += 1
+   */
+
+   //int index = 0;
+
+   //outer loop is over each row of A
+   #pragma omp parallel for
+   for(int row = 0; row < n; row++){
+      double sum = 0.0;
+
+      //inner loop is dot product of whatever row of A you're on
+      for(int column = 0; column < n; column++){
+
+         //since its row major format index = col + row * N
+         //index = column + row * n;
+
+         //index * the vector computation is the dot product we will accumulate for each row
+         sum += A[column + row * n] * x[column];
+      }
+      
+      //y = y + A * x (sum is the holder for this)
+      //here is where we accumulate, this is the y + part in y = y + A * x
+      y[row] += sum;
+   }
+
 
 }
 
